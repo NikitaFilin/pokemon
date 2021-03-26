332fb748e25911697d4201d525b0b424c55b067d
NikitaFilin <9774616923@mail.ru>
2021-03-26 15:49:53 +0300

    добавлен Router

---
+++ src/components/Error.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Error.css";
+
+export const Error = () => {
+  return (
+    <div className="error">
+      <h1>ERROR 404</h1>
+      <p>Мы еще не поймали этого покемона</p>
+    </div>
+  );
+};
